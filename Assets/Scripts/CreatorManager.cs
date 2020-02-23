dbed01beffddd10e7801855009de53557b61c3b0
Oliver Findra <o.findra.svk@gmail.com>
2020-02-23 16:25:24 +0100

    Basic controls and test prefab added

---
+++ Assets/Scripts/CreatorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatorManager: MonoBehaviour
+{
+    [SerializeField] private GameObject testPrefab;
+    [SerializeField] private InputManager inputManager;
+    public GameObject model;
+
+    private void OnEnable()
+    {
+        InputManager.onRotateModel += RotateModel;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.onRotateModel -= RotateModel;
+    }
+
+    void Start() {
+        model = GameObject.Instantiate(testPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+    void Update() {
+        
+    }
+
+    void RotateModel(Vector3 rotation)
+    {
+        model.transform.Rotate(rotation, 1f);
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager: MonoBehaviour {
+    // Start is called before the first frame update
+
+    [SerializeField] private Button importBtn;
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Text modelNameText;
+    
+    void Start() {
+        importBtn.onClick.AddListener(ImportModel);
+        startBtn.onClick.AddListener(StartCreation);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void ImportModel() {
+        Debug.Log("Import model - not functional yet!");
+    }
+
+    private void StartCreation() {
+        Debug.Log("Start creation");
+        SceneManager.LoadScene("CreatorScene");
+    }
+}
